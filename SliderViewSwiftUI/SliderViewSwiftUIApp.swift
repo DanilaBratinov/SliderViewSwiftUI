be6166115519475cbfce349afa7ed1b6cecd934b
DanilaBratinov <bratinov0704@inbox.ru>
2023-01-14 16:40:49 +0300

    Initial Commit

---
+++ SliderViewSwiftUI/SliderViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderViewSwiftUIApp.swift
+//  SliderViewSwiftUI
+//
+//  Created by Данила Братинов on 14.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SliderViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
